7a595f730bdeefea11d6b3abb07c30319f9f4c0d
ripon1995 <ripon1995dc@gmail.com>
2023-06-01 15:02:08 +0600

    what?  - app bar color changed based on the environment

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
+import 'flavors.dart';
 
 void main_app() {
+  Color appBarColor = Colors.black;
+  if (F.appFlavor == Flavor.DEV) {
+    appBarColor = Colors.blue;
+  } else if (F.appFlavor == Flavor.STG) {
+    appBarColor = Colors.green;
+  } else if (F.appFlavor == Flavor.PROD) {
+    appBarColor = Colors.pink;
+  }
+
   runApp(
     GetMaterialApp(
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
-      color: Colors.red,
-
+      color: Colors.blue,
+      theme: ThemeData(appBarTheme: AppBarTheme(backgroundColor: appBarColor)),
     ),
   );
 }
